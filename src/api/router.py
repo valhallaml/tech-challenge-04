44f8ec74722bd63a63fcc4168bd398090f3d3ee1
Jackson Castro <jackson.castro@sefin.fortaleza.ce.gov.br>
2025-06-05 21:42:57 -0300

    Add predict router

--- src/api/router.py
+++ src/api/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from api.v1.football import router
+from src.api.v1.finance import router
 
 api_router = APIRouter()
 api_router.include_router(router, prefix='/finance', tags=[ 'finance' ])
--- src/api/v1/finance.py
+++ src/api/v1/finance.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
+@router.get('/predict')
+async def get_predict():
+    return 'Predict'
+
 @router.get('/train')
 async def get_train():
     return 'Train'
